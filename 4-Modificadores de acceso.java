22afe847920b1ce76f1a97c74ed1b0953f8a5029
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 15:49:32 -0300

    Create 4-Modificadores de acceso.java

---
+++ 4-Modificadores de acceso.java
@@ -0,0 +1,6 @@
+
+  int numero;   // Cualquier clase que este en su mismo paquete podra acceder 
+
+  public int numero;  // Cualquier clase que este en su mismo paquete o no, podra acceder 
+
+  private int numero;  // Solo su misma clase podra acceder
