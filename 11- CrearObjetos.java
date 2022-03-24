98f86dd58cdf192eb061c0429cf79a0434134ca7
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 17:50:36 -0300

    Create 11- CrearObjetos.java

---
+++ 11- CrearObjetos.java
@@ -0,0 +1,10 @@
+
+// Previamente tenemos la clase Auto, que nos va a servir de plantilla
+    
+// 1. Creamos una nueva clase con su metodo main
+          
+    public class AutoMain{
+      public static void main (String[] args){
+          Auto autoObj = new Auto();
+      }
+    }
