4e7cb26e75bbd00096ba3673c4fbd91d1c679cfe
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 17:22:16 -0300

    Create 9-SwitchCase.java

---
+++ 9-SwitchCase.java
@@ -0,0 +1,31 @@
+
+
+
+
+  String clima = "soleado";
+
+      switch (clima) {                                                // IMPORTANTE usar Break despues de cada caso
+        case "soleado":
+            System.out.println("El dia esta soleado");
+            break;
+          
+        case "nublado":
+            System.out.println("El dia esta nublado");
+            break;
+          
+        case "lluvia":
+            System.out.println("Esta lloviendo");
+            break;      
+          
+        default:                                                          // Caso por defecto
+            System.out.println("No se pudo identificar el clima");
+            break;
+      }
+
+
+
+
+
+
+
+
