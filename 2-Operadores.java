5cef051558decf18596f247167a9a80472915a53
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 15:24:57 -0300

    Create 2-Operadores.java

---
+++ 2-Operadores.java
@@ -0,0 +1,35 @@
+// OPERADORES
+					
+			// Aritmeticos
+					int resultado = numero1 + numero2;
+					int resultado = numero1 -  numero2;
+					int resultado = numero1 *  numero2;
+					int resultado = numero1 /  numero2;
+					int resultado = numero1 % numero2;
+					
+		   // Logicos
+					
+					> Mayor que
+					>= Mayor o igual que
+					< Menor que
+					<= Menor o igual que
+					== Numero igual que
+					!= Numero distinto que
+					&& AND
+					|| OR
+					! NOT (Negacion)
+					
+			// Asignacion
+					= 
+					+=
+					-=
+					/=
+					*=
+					%=
+					
+			// Incremento
+					numero++
+			// Decremento
+					numero--
+			// Concatenacion
+					numero+
