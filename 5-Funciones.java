1b0ad053ce31b32d4fc2f1d016728a00a3d2ff13
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 15:57:40 -0300

    Create 5-Funciones.java
    Sintaxis funciones

---
+++ 5-Funciones.java
@@ -0,0 +1,16 @@
+
+  holaMundo();
+    private static void holaMundo(){
+      Syatem.out.println("Hola Mundo!");
+     }
+
+  
+  holaUser("Ale");
+    private static void holaUser(String nombre){
+      System.out.println("Hola, "+ nombre + "!");
+    }
+
+  returnHola("Ale");
+    private static String returnHola(String nombre){
+      return ("Hola, "+ nombre + "!");
+    }
