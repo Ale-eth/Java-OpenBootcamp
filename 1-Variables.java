323fd106eef3e0c677a15525d986fc77745bce05
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 15:24:16 -0300

    Update and rename 1-Conceptos.java to 1-Variables.java

---
+++ 1-Variables.java
@@ -0,0 +1,25 @@
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");   // Imprimir por consola
+		
+		// VARIABLES
+			// Numeros enteros:
+					byte numero1 = 1;  //Ocupa 1byte de memoria
+					short numero2 = 2; //Ocupa 2byte de memoria
+					int numero3 =  3;  	//Ocupa 3byte de memoria
+					long numero4 =  4L;  //Ocupa 4byte de memoria
+					
+			// Numeros decimales
+					float numero5 = 5.9f;
+					double numero6 = 9.89d;
+					
+			// Caracter
+					char caracter = '@';
+			
+			// Booleanos
+					boolean verdadero = true;
+					boolean falso = false;
+					
+			// Textos
+					String nombre = "Alejandro";
+					
