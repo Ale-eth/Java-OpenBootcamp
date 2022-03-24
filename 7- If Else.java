5349150871329680b30df78e2b8aba669e4c1f4e
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 16:40:31 -0300

    Update 7- If Else.java

--- 7- If Else.java
+++ 7- If Else.java
@@ -1,1 +1,25 @@
 
+
+
+
+      int numero1 = 50;
+      int numero2 = 100;
+
+          if (numero1 > numero2){                                       // Si la condicion es verdadera
+              System.out.println(numero1 +" es mayor que "+ numero2);    // Corre este bloque de codigo
+          }
+
+          
+
+          if (numero1 > numero2){
+              System.out.println(numero1 +" es mayor que "+ numero2);    // Si la condicion es verdadera, corre este bloque de codigo
+          }else{                                                              // sino
+              System.out.println(numero1 +" no es mayor que "+ numero2);  // Ejecuta este otro bloque de codigo
+          }
+                
+
+
+
+           if (numero1 > numero2  &&  numero2 > numero3){                 // Mismo caso pero con 2 condiciones, si se cumplen ambas, es verdadero
+              System.out.println(numero1 +" es mayor que "+ numero2);     // && and || or
+          }
