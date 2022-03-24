2e3150eb9ca5ffdfac53436844b84425b229a267
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 17:39:12 -0300

    Create 10- ClasesPOO.java

---
+++ 10- ClasesPOO.java
@@ -0,0 +1,28 @@
+
+
+    // Las clases son "moldes" para imitar figuras de la realidad, nos permiten crear objetos (POO)
+
+
+    public class Auto {
+        
+        // Atributos
+        String marca;
+        String color;
+        String patente;
+        Double peso_kg;
+        Integer velocidad = 0;
+      
+      
+        // Metodos constructores
+        public Auto(String marca, String color, String patente, Double peso_kg){
+          this.marca = audi;              // this hace referencia a la propiedad (en este caso color) del objeto que se crea
+          this.color = negro;
+          this.patente = "4AJ78K";
+          this.peso_kg = 890:
+        }
+      
+        // Comportamiento
+        public void acelerar(Integer cantidad){
+          this.velocidad += cantidad;
+          }
+    }
