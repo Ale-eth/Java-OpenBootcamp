629e61aea36f5f9f5c80c98062946d270de11953
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 16:25:40 -0300

    Update 6- Sobrecarga de funciones.java

--- 6- Sobrecarga de funciones.java
+++ 6- Sobrecarga de funciones.java
@@ -1,5 +1,25 @@
+    
 
+            // A simple vista puede parecer que esto es incorrecto ya que las 3 funciones tienen el mismo nombre, pero no.
+           //  Al tener distintos parametros, no hay problema. A esto se llama sobrecarga de funciones 
 
   
+    holaMundo();
+  
+    holaMundo("Ale");
+
+    holaMundo("Ale", "eth");
+
+
+
     private static void holaMundo(){
+      System.out.println("Hola Mundo!");
+    }
+
+    private static void holaMundo(String nombre){
+      System.out.println("Hola " + nombre + "!");
+    }
+
+    private static void holaMundo(String nombre, String apellido){
+      System.out.println("Hola " + nombre + " " + apellido + "!");
     }
