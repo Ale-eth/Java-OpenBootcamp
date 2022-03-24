10f6667b48af8fcaa555b201287948f8d3e76e57
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-24 16:00:49 -0300

    Update 4-Modificadores de acceso.java

--- 4-Modificadores de acceso.java
+++ 4-Modificadores de acceso.java
@@ -4,3 +4,23 @@
   public int numero;  // Cualquier clase que este en su mismo paquete o no, podra acceder 
 
   private int numero;  // Solo su misma clase podra acceder
+
+
+
+  // Quien puede acceder?
+
+public :    - Su misma clase
+            - Otra clase del mismo paquete
+            - Subclase de otro paquete
+            - Otra clase de otro paquete
+
+
+protected : - Su misma clase
+            - Otra clase del mismo paquete
+            - Subclase de otro paquete
+ 
+
+default :   - Su misma clase
+            - Otra clase del mismo paquete
+
+private :   - Su misma clase
