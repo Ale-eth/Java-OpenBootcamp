3af8926d3aeb56e387d37f41615f7b57fa3501e1
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-25 17:46:00 -0300

    Update 16- Clases Abstractas.java

--- 16- Clases Abstractas.java
+++ 16- Clases Abstractas.java
@@ -1,1 +1,19 @@
 
+
+
+      // Las clases abstractas no se pueden instanciar, por lo general se usan cuando aplicamos herencias
+
+      public abstract class Auto{     // Para indicar que la clase es abstracta, escribimos abstract antes del class
+        String color;
+        String modelo;
+        String patente;
+      }
+
+      // Suele usarse para forzar al desarrollador a usar una clase hija de esa clase abstracta
+      // Ej:
+      //        Auto: abstract class
+      //        AutoElectrico: public class
+      //        AutoHibrido: public class
+  
+  
+      // Podremos instranciar AutoElectrico y AutoHibrido que son hijas de la clase Auto, pero no podremos instanciar Auto solo.
