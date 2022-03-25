ec1692c209ff4b7acf308572a17d91b2304fa93a
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-25 18:31:35 -0300

    Update 16- Clases Abstractas.java

--- 16- Clases Abstractas.java
+++ 16- Clases Abstractas.java
@@ -1,7 +1,8 @@
 
 
 
-      // Las clases abstractas no se pueden instanciar, por lo general se usan cuando aplicamos herencias
+      // Las clases abstractas no se pueden instanciar, se usan solo en superclases (es decir que tienen clases hijas)
+      // y deben contener como minimo un metodo abstracto.
 
       public abstract class Auto{     // Para indicar que la clase es abstracta, escribimos abstract antes del class
         String color;
@@ -17,6 +18,15 @@
   
   
       // Podremos instranciar AutoElectrico y AutoHibrido que son hijas de la clase Auto, pero no podremos instanciar Auto solo.
+
+
+
+      // Ejemplo:
+      //                clase:            SerVivo                      metodo: alimentarse();
+      //                subclase:         Planta y Animal              metodo: alimentarse();
+      //                subclase Animal:  Carnivoro y Herbivoro        metodo: alimentarse();
+
+      // Todos son seres vivos y se alimentan, pero no de la misma manera
 
 
 
