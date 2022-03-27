f9c561a0ab0246e7063f5d97f8112af2b676eed3
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-27 17:32:21 -0300

    Create 19- Arrays.java

---
+++ 19- Arrays.java
@@ -0,0 +1,16 @@
+
+
+
+    // Variables
+    String nombre1 = "Juan";
+    String nombre2 = "Pedro";
+    String nombre3 = "Jose";
+
+    // Declarar un arreglo
+    String[] nombres = new String[3];
+    String[] nombres2 = new String[]{nombre1, nombre2, nombre3}
+
+    // Recorrer un arreglo
+    for (int i=0; i < arreglo.length; i++){
+      System.out.println("Posicion: "+i);
+    }
