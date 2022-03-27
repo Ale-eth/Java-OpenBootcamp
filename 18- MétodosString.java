a474cf654d2c4aff60cf07266b96ca24c4fc1414
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-27 17:05:37 -0300

    Create 18- MétodosString.java

---
+++ 18- MétodosString.java
@@ -0,0 +1,11 @@
+    
+
+
+    String frase = "Hola Mundo!";
+
+
+    frase.lenght()  // Devuelve la cantidad de caracteres que tiene el string
+    frase.toUpperCase()  // Devuelve el string en mayusculas
+    frase.trim()  // Devuelve el string sin espacios al inicio ni al final
+    frase.equals(frase2)  // Devuelve T o F, si el string es igual al otro string
+    frase.equalsIgnoreCase(frase2)  // Lo mismo que el anterior pero ignorando mayus y minus
