7efc6fa3fd03b59b3d7527f050d3a7c24766b3a8
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-03-27 18:55:13 -0300

    Update 24- Scanner.java

--- 24- Scanner.java
+++ 24- Scanner.java
@@ -8,4 +8,19 @@
  import java.util.Scanner;
  
     public static void main (String[] args){
+        
+        Scanner scanner = new Scanner(System.in);           // Creamos el objeto scanner
+        
+        System.out.println("Introduzca su nombre: ");
+        String nombre = scanner.nextLine();                 // Va a guardar el valor que ingresemos por consola
+        
+        System.out.println("Introduzca su edad");
+        int numero = scanner.nextInt();
     }
+
+
+
+
+
+
+
